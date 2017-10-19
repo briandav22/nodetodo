e4330858bb2f2af3e4de207b1682a0288b73fdee
Brian Davenport <briand@plxr.local>
2017-10-19 13:10:33 -0400

    add test cases

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
-
+const {ObjectID} = require('mongodb')
 const {app} = require('./../server');
 const {Todo} = require('./../models/todos');
 
 const todos = [{
+  _id:new ObjectID(),
   text: 'first test todo'},
-  {text: 'second test todu'
+  {
+    _id:new ObjectID(),
+    text: 'second test todu'
 }];
 
 beforeEach((done)=>{
@@ -69,3 +72,30 @@ describe('Get /todos', ()=> {
       .end(done);
   });
 });
+
+describe('GET /todos/:id', () => {
+  it('should return todo doc', (done) => {
+    request(app)
+      .get(`/todos/${todos[0]._id.toHexString()}`)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(todos[0].text);
+      })
+      .end(done)
+
+  });
+
+  it('should return 404', (done) => {
+    var hexID = new ObjectID().toHexString();
+    request(app)
+      .get(`/todos/${hexID}`)
+      .expect(404)
+      .end(done);
+  });
+  it('should return 404 for non-object is', (done) => {
+    request(app)
+      .get('/todos/123abc')
+      .expect(404)
+      .end(done);
+  })
+})
