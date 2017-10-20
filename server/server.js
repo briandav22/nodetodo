948939f7ca547124aacc4a484165616dfce56541
briandav22 <briandav22@gmail.com>
2017-10-20 15:43:23 -0400

    fixiing

--- server/server.js
+++ server/server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var express = require('express');
 var bodyParser = require('body-parser');
 var {ObjectID} = require ('mongodb');
@@ -70,63 +69,3 @@ app.delete('/todos/:id', (req, res) => {
 app.listen(port, () => {
   console.log(`started on port ${port}`)
 })
-
-module.exports = {app};
-=======
-var express = require('express');
-var bodyParser = require('body-parser');
-var {ObjectID} = require ('mongodb');
-
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todos');
-var {users} = require('./models/users');
-
-var app = express();
-
-const port = process.env.PORT || 3000;
-
-app.use(bodyParser.json());
-
-app.post('/todos', (req,res) => {
-  var todo = new Todo({
-    text: req.body.text
-  });
-  todo.save().then((doc)=>{
-    res.send(doc);
-  },(e) => {
-    res.status(400).send(e);
-  })
-});
-
-app.get('/todos', (req,res) => {
-  Todo.find().then((todos)=>{
-    res.send({todos})
-  }, (e)=>{
-    res.status(400).send(e);
-  });
-});
-
-app.get('/todos/:id',(req, res) => {
-  var id = req.params.id;
-
-  if (!ObjectID.isValid(id)){
-    return res.status(404).send();
-  }
-  Todo.findById(id).then((todo) =>{
-    if (!todo){
-      return res.status(404).send();
-    }
-
-    res.send({todo});
-  }).catch((e)=> {
-    res.status(400).send();
-  });
-
-});
-
-app.listen(port, () => {
-  console.log(`started on port ${port}`)
-})
-
-module.exports = {app};
->>>>>>> 5485ea28c4be4a7713283011317e3de5ee4ec579
